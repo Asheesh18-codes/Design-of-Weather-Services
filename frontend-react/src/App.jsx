9ad081627e7fc12e597f0e2359ecd7ab5195a1d9
Devansh <devanshjhaa@gmail.com>
2025-09-25 23:50:01 +0530

    changed folder

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-sky-100">
+      <h1 className="text-4xl font-bold text-sky-600">
+        ✅ Tailwind v4 is working!
+      </h1>
+    </div>
+  )
+}
